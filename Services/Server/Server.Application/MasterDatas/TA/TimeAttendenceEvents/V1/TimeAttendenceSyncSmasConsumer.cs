1b2af039b7008b4ce00ebfc0fca8ebf9a8789c26
huyld1 <huyld@acs.vn>
2024-11-06 15:46:55 +0700

    cập nhật hiển thị

--- Services/Server/Server.Application/MasterDatas/TA/TimeAttendenceEvents/V1/TimeAttendenceSyncSmasConsumer.cs
+++ Services/Server/Server.Application/MasterDatas/TA/TimeAttendenceEvents/V1/TimeAttendenceSyncSmasConsumer.cs
@@ -35,26 +35,26 @@ public class TimeAttendenceSyncSmasConsumer : IConsumer<SyncDataRequest>
         {
             var datas = context.Message;
             Logger.Information("Consumer:" + datas);
+
             var res = await _smartService.PostSyncAttendence2Smas(datas);
 
-            var item = new TA_TimeAttendenceSync()
+            if (res != null)
             {
-                Id = datas.Id,
-            };
-
-            if (res != null && res.IsSuccess)
-            {
-                string response = JsonConvert.SerializeObject(res);
-                item.SyncStatus = res.Responses[0].status;
-                item.Message = res.Responses[0].message;
-                item.ParamResponses = response;
+                var item = new TA_TimeAttendenceSync() { Id = datas.Id, };
+                if (res.IsSuccess)
+                {
+                    string response = JsonConvert.SerializeObject(res);
+                    item.SyncStatus = res.Responses[0].status;
+                    item.Message = res.Responses[0].message;
+                    item.ParamResponses = response;
+                }
+                else
+                {
+                    item.SyncStatus = res.IsSuccess;
+                    item.Message = res.Message;
+                }
+                await _timeAttendenceEventService.SaveStatuSyncSmas(item);
             }
-            else
-            {
-                item.SyncStatus = res.IsSuccess;
-                item.Message = res.Message;
-            }
-            var retval = await _timeAttendenceEventService.SaveStatuSyncSmas(item);
         }
         catch (Exception ex)
         {
