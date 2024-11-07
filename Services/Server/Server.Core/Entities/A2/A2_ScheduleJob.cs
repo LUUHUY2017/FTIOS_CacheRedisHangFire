35cb36ab7f87fa8244f77a799e4cde8ee99d5149
huyld1 <huyld@acs.vn>
2024-11-07 17:10:11 +0700

    Cập nhật thêm bảng Job lập lịch

---
+++ Services/Server/Server.Core/Entities/A2/A2_ScheduleJob.cs
@@ -0,0 +1,30 @@
+﻿using Shared.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Core.Entities.A2;
+
+[Table("A2_ScheduleJob")]
+public class A2_ScheduleJob : EntityBase
+{
+    [MaxLength(500)]
+    public string? ScheduleJobName { get; set; }
+    /// <summary>
+    /// Thời gian gửi báo cáo
+    /// </summary>
+    public TimeSpan? ScheduleTimeSend { get; set; }
+    /// <summary>
+    /// Tuần suất gửi: 1 phút 1 lần, 1 ngày 1 lần, 
+    /// </summary>
+    /// 
+    [MaxLength(250)]
+    public string? ScheduleSequentialSending { get; set; }
+
+    /// <summary>
+    /// Loại đồng bộ: Học sinh, Điểm danh
+    /// </summary>
+    [MaxLength(250)]
+    public string? Scheduleype { get; set; }
+    [MaxLength(500)]
+    public string? ScheduleNote { get; set; }
+}
---
+++ Services/Server/Server.Core/Entities/A2/A2_ScheduleJobLog.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Core.Entities.A2;
+
+[Table("A2_ScheduleJobLog")]
+public class A2_ScheduleJobLog : EntityBase
+{
+    [MaxLength(500)]
+    public string? ScheduleJobId { get; set; }
+    public bool? ScheduleJobStatus { get; set; }
+    [MaxLength(500)]
+    public string? Message { get; set; }
+
+    public string? ScheduleJobParams { get; set; }
+
+    [MaxLength(500)]
+    public string? ScheduleLogNote { get; set; }
+}
