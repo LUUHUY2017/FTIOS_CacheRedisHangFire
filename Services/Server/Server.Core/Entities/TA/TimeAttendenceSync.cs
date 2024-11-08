8876cb615c4c2fd885ad59300c22e27c008b051e
LeHuynhDuc02Work <ducle1701work@gmail.com>
2024-11-08 08:54:32 +0700

    Fix bug conflict

--- Services/Server/Server.Core/Entities/TA/TimeAttendenceSync.cs
+++ Services/Server/Server.Core/Entities/TA/TimeAttendenceSync.cs
@@ -1,13 +1,12 @@
 ﻿using Shared.Core.Entities;
 
-namespace Server.Core.Entities.TA
+namespace Server.Core.Entities.TA;
+
+public class TimeAttendenceSync : EntityBase
 {
-    public class TimeAttendenceSync : EntityBase
-    {
-        public string? TimeAttendenceEventId { get; set; }
-        public string? ParamRequests { get; set; }
-        public string? ParamResponses { get; set; }
-        public bool? SyncStatus { get; set; }
-        public string? Message { get; set; }
-    }
+    public string? TimeAttendenceEventId { get; set; }
+    public string? ParamRequests { get; set; }
+    public string? ParamResponses { get; set; }
+    public bool? SyncStatus { get; set; }
+    public string? Message { get; set; }
 }
