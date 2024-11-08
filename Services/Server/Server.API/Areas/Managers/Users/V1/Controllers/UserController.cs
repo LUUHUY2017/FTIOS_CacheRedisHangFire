056bb34d3fa30ea0b880e0bed2a0ebef8c99bc84
LeHuynhDuc02Work <ducle1701work@gmail.com>
2024-11-08 11:20:11 +0700

    Author api

--- Services/Server/Server.API/Areas/Managers/Users/V1/Controllers/UserController.cs
+++ Services/Server/Server.API/Areas/Managers/Users/V1/Controllers/UserController.cs
@@ -19,8 +19,7 @@ using Shared.Core.Identity;
 namespace Server.API.Areas.Managers.Users.V1.Controllers;
 
 [Route("v1/[controller]")]
-//[Authorize("Bearer")]
-//[AuthorizeMaster(Roles = RoleConst.MasterDataPage)]
+[AuthorizeMaster]
 public class UserController : Controller
 {
     const string pathUrl = "~/Areas/Managers/Users/V1/Views/";
