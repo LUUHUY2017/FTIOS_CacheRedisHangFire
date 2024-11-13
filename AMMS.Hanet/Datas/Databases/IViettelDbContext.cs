507f8d87326a535fc1eaa6207dc7fc77e5ba5bdb
ACS.Quyết <quyet@acs.vn>
2024-11-13 14:49:39 +0700

    Project Hanet Bổ sung DB Context để kết nối bảng students

---
+++ AMMS.Hanet/Datas/Databases/IViettelDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AMMS.Hanet.Datas.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMMS.Hanet.Datas.Databases;
+
+public interface IViettelDbContext
+{
+    public DbSet<Student> Student { get; set; }
+
+    Task<int> SaveChangesAsync();
+}
