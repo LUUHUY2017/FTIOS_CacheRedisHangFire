c26e1d1914466258ec1883bb35fdb8d87a23a2d3
huyld1 <huyld@acs.vn>
2024-11-20 17:33:52 +0700

    Cập nhật nút lấy lại dữ liệu điểm danh trên thiết bị

---
+++ Services/Server/Server.Application/MasterDatas/A2/Devices/Models/DeviceSyncAttendenceRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Server.Application.MasterDatas.A2.Devices.Models
+{
+    public class DeviceSyncAttendenceRequest
+    {
+        public string? Id { get; set; }
+        public string? OrganizationId { get; set; }
+        public string? SerialNumber { get; set; }
+        public string? DeviceName { get; set; }
+        public string? DeviceType { get; set; }
+        public string? DeviceModel { get; set; }
+
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+
+
+
+
+    }
+}
